35aa0b5e6ffb4c54fe0c82b21ee8560100d1034c
Fernando Ludvig <fernandoludvig0804@gmail.com>
2025-09-02 20:12:19 -0300

    JavaScript  Basics

---
+++ js-basics/index.js
@@ -0,0 +1,39 @@
+// Variables
+//let name = 'Mosh'; // String
+// let age = 30; // Number
+//let isApproved = true; or false // Booelean
+// let firstName = undefined;
+//let selectedColor = null;
+//console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a namber (1name)
+// cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+// Constants
+//const interestRate = 0.3;
+//console.log(interestRate);
+
+// Objects
+//let person = {
+//    name: 'Mosh',
+//    age: 30
+//};
+//Dot Notatiton
+//person.name = 'John';
+
+//Bracket Notation
+//person['name'] = 'Mary';
+
+//console.log(person.name);
+
+// Arrays
+
+//let selectedColors = ['red', 'blue'];
+//console.log(selectedColors[0]); vai trazer somente o valor do primeiro item
+
+//let selectedColors = ['red', 'blue'];
+//selectedColors[2] = 'blue';
+//console.log(selectedColors); vai adicionar a cor azul
